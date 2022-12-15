b3992d5ee195b6bc4783d47ceaf3313a9ac30dd2
ntno <ntno@users.noreply.github.com>
2022-12-15 18:33:21 -0500

    formatting and print statement

--- index.js
+++ index.js
@@ -4,27 +4,28 @@ const tc = require('@actions/tool-cache');
 const { getDownloadObject } = require('./lib/utils');
 
 async function setup() {
-  try {
-    // Get version of tool to be installed
-    const version = core.getInput('version');
+    try {
+        // Get version of tool to be installed
+        const version = core.getInput('version');
+        console.log(`buildenv version to install: ${version}!`);
 
-    // Download the specific version of the tool, e.g. as a tarball/zipball
-    const download = getDownloadObject(version);
-    const pathToTarball = await tc.downloadTool(download.url);
+        // Download the specific version of the tool, e.g. as a tarball/zipball
+        const download = getDownloadObject(version);
+        const pathToTarball = await tc.downloadTool(download.url);
 
-    // Extract the tarball/zipball onto host runner
-    const extract = download.url.endsWith('.zip') ? tc.extractZip : tc.extractTar;
-    const pathToCLI = await extract(pathToTarball);
+        // Extract the tarball/zipball onto host runner
+        const extract = download.url.endsWith('.zip') ? tc.extractZip : tc.extractTar;
+        const pathToCLI = await extract(pathToTarball);
 
-    // Expose the tool by adding it to the PATH
-    core.addPath(path.join(pathToCLI, download.binPath));
-  } catch (e) {
-    core.setFailed(e);
-  }
+        // Expose the tool by adding it to the PATH
+        core.addPath(path.join(pathToCLI, download.binPath));
+    } catch (e) {
+        core.setFailed(e);
+    }
 }
 
 module.exports = setup
 
 if (require.main === module) {
-  setup();
+    setup();
 }
