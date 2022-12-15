c2aa3ae53084116af6fbf66095756800cc4adae1
ntno <ntno@users.noreply.github.com>
2022-12-15 18:51:50 -0500

    report path to cli

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ async function setup() {
         // Extract the tarball/zipball onto host runner
         const extract = download.url.endsWith('.zip') ? tc.extractZip : tc.extractTar;
         const pathToCLI = await extract(pathToTarball);
+        console.log(`pathToCLI: ${ pathToCLI }`)
 
         // Expose the tool by adding it to the PATH
         core.addPath(path.join(pathToCLI, download.binPath));
